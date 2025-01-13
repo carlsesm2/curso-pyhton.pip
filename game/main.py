b6c0cbdd5b10cf902925a83ec900ceb1744245f5
carlsesm2 <eduardocesm19@gmail.com>
2025-01-13 17:09:52 -0400

    save game

---
+++ game/main.py
@@ -0,0 +1,78 @@
+import random
+
+
+def choose_options():
+    options = ('piedra', 'papel', 'tijera')
+    user_option = input('piedra, papel o tijera => ')
+    user_option = user_option.lower()
+
+    if not user_option in options:
+        print('Esa opcion no es valida')
+    
+    computer_option = random.choice(options)
+
+    print('User option => ', user_option)
+    print('Computer option => ', computer_option)
+    
+    return user_option, computer_option
+
+def check_rules(user_option, computer_option, user_wins, computer_wins):
+    if user_option == computer_option:
+        print('Empate')
+    elif user_option == 'piedra':
+        if computer_option == 'tijera':
+            print('piedra gana a tijera')
+            print('user ganó!')
+            user_wins += 1
+        else:
+            print('Papel gana a piedra')
+            print('computer gano!')
+            computer_wins += 1
+    elif user_option == 'papel':
+        if computer_option == 'piedra':
+            print('papel gana a piedra')
+            print('user gano')
+            user_wins += 1
+        else:
+            print('tijera gana a papel')
+            print('computer gano!')
+            computer_wins += 1
+    elif user_option == 'tijera':
+        if computer_option == 'papel':
+            print('tijera gana a papel')
+            print('user gano!')
+            user_wins += 1
+        else:
+            print('piedra gana a tijera')
+            print('computer gano!')
+            computer_wins += 1
+    return user_wins, computer_wins
+
+def check_winner(user_wins, computer_wins):
+    if computer_wins == 2:
+        print('el ganador es la computadora')
+        exit()
+    if user_wins == 2:
+        print('El ganador es el usuario')
+        exit()
+
+def run_game():
+    computer_wins= 0
+    user_wins = 0   
+    rounds = 1  
+
+    while True:
+        print('*' * 10)
+        print('ROUND', rounds)
+        print('*' * 10)
+        rounds += 1
+
+        print('computer wins ', computer_wins)
+        print('user wins ', user_wins)
+
+        user_option, computer_option = choose_options()
+        user_wins, computer_wins = check_rules(user_option, computer_option, user_wins, computer_wins)
+        check_winner(user_wins, computer_wins)
+
+
+run_game()
